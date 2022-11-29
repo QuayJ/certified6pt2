c7b5c3104202b1d02fc1200cdf94aa6d3e89c70a
Quayj <sjw.wilson@icloud.com>
2022-11-29 11:58:19 -0500

    Start of mod 7

---
+++ JS/script.js
@@ -0,0 +1,36 @@
+//below is for is home legend
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+//Below is for carousel
+
+const buttons = document.querySelectorAll("[data-carousel-button]")
+
+buttons.forEach(button => {
+buttons.addEventListener("click", () => {
+        const offset = button.dataset.carousel === "next" ? 1 : -1
+        const slides = button 
+            .closest("[data-carousel]")
+            .querySelector("[data-slides]")
+        const  activeSlide = slides.querySelector("[data-active]")
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset
+        if (newIndex < 0) newIndex = slides.children.length - 1
+        if (newIndex >= slides.children.length) newIndex = 0
+
+        slides.children[newIndex].dataset.active = true
+        delete activeSlide.dataset.active
+
+    })
+})
