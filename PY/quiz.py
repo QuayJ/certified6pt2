16939c4638329bc635a2683b76aff9b71e360642
Quayj <sjw.wilson@icloud.com>
2022-12-01 09:57:23 -0500

    working on new Artist page, working on form

---
+++ PY/quiz.py
@@ -0,0 +1,38 @@
+print("How well do you know Drake?")
+
+playing = input("Lets play! ")
+
+if playing != "yes" "ok":
+    quit()
+
+print("Okay! Lets go!")
+
+answer = input("What was Drakes first studio album? ")
+if answer == "first album":
+    print('Correct!')
+else:
+    print("Thats not right!")
+
+answer = input(" ")
+if answer == "":
+    print('Correct!')
+else:
+    print("Thats not right!")
+
+answer = input(" ? ")
+if answer == " ":
+    print('Correct!')
+else:
+    print("Thats not right!")
+
+answer = input(" ? ")
+if answer == " ":
+    print('Correct!')
+else:
+    print("Thats not right!")
+
+answer = input(" ? ")
+if answer == " ":
+    print('Correct!')
+else:
+    print("Thats not right!")
