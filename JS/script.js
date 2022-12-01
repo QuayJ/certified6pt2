341b637e3f0536740758a26d0c88f804db23849f
Quayj <sjw.wilson@icloud.com>
2022-12-01 11:53:03 -0500

    adding JS to artist.html, made a mock page, need to work on graph and form box

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,19 @@
+//Below is for slider on testArtist.html
+
+const tl = gsap.timeline({ defaults: { ease: 'power1.out' } });
+
+tl.to(".text", {y:"0%", duration: 1, stagger: 0.25});
+tl.to(".slider", {y: "-100%", duration: 1.5, delay: 0.5});
+tl.to(".intro1", {y: "-100%", duration: 1}, "-=1");
+
+// tl.to(".text2", {y:"0%", duration: 1, stagger: 0.25});
+tl.to(".slider", {y: "100%", duration: 1.5, delay: 0.5});
+// tl.to(".intro1", {y: "-100%", duration: 1}, "-=1");
+
+tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 1});
+tl.fromTo('.big-text', {opacity: 0}, {opacity: 1, duration: 1}, '-=1');
+
+
 //below is for is home legend
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
